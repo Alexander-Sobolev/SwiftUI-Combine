593fb82398bdf3b3064aeffd5379e892a0157c14
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-06-27 18:13:35 +0300

    Commit

---
+++ AppSwiftUI/Views/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  AppSwiftUI
+//
+//  Created by Alexander Sobolev on 27.06.2021.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView(image: Image("turtlerock"))
+    }
+}
