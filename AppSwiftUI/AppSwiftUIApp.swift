3087bb64ca8dcbafd9eec61cca69dba8ef47242b
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-06-27 14:13:46 +0300

    Initial Commit

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Alexander Sobolev on 27.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSwiftUI
+//
+//  Created by Alexander Sobolev on 27.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
