d253d26a95e1d50e1d68e959f160476c7c419388
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-07-01 08:28:27 +0300

    Changes

--- AppSwiftUI/Views/SwiftUIView.swift
+++ AppSwiftUI/Views/SwiftUIView.swift
@@ -14,7 +14,7 @@ struct SwiftUIView: View {
         image
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 7)
+            .shadow(radius: 6)
     }
 }
 
